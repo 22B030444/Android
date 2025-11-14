958ff0bf85e68c5ff737443629327e0f34184f5c
Жасмин <zh_suleimenova@kbtu.kz>
2025-11-14 10:46:44 +0500

    practice4

--- assignment4/app/src/main/java/com/example/assignment4/UiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.assignment4
-
-sealed class UiState {
-    object Loading : UiState()
-    data class Success(val posts: List<Post>) : UiState()
-    data class Error(val message: String) : UiState()
-
-}
